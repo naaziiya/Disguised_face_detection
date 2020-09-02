a06b9e9201ccbd0b84a88dd485152e3831ac6abb
Naziya M <63766730+naaziiya@users.noreply.github.com>
2020-09-02 07:43:21 +0530

    Add files via upload

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Naazu\Project\disguised_face\disguised_face\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
